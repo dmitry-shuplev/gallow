7509d8853ca0ffb3f03b8267705af6101707d493
dmitry-shuplev <dmitrijsuplev@gmail.com>
2025-02-02 19:10:46 +0300

    Current work (#1)
    * divide_classes
    
    * chenge_method_interface_to_show
    
    * rename_vocabular
    
    * new_branch_add

--- src/Main.java
+++ src/Main.java
@@ -5,26 +5,26 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) {
-        Gallow gallow1 = new Gallow();
-        gallow1.convencedWord();
-        gallow1.generateUnhiddenWord();
+        GameSession game = new GameSession();
+        game.convencedWord();
+        game.generateUnhiddenWord();
 
         while (true) {
-            interfase(gallow1);
-            compareResult(gallow1);
+            Show.showGallow(game);
+            compareResult(game);
             char inputedChar = getChar();
-            compareChars(gallow1, inputedChar);
+            compareChars(game, inputedChar);
         }
     }
 
-    static void compareResult(Gallow gallow1) {
-        if (gallow1.hiddenWord.equals(gallow1.unhideenWord)) {
+    static void compareResult(GameSession game) {
+        if (game.hiddenWord.equals(game.unhideenWord)) {
             System.out.println("Вы угадали слово.");
             System.exit(0);
         }
-        if (gallow1.errors == 6) {
+        if (game.errors == 6) {
             System.out.println("Вы проиграли.");
-            System.out.println("Было загадано слово:" + gallow1.hiddenWord);
+            System.out.println("Было загадано слово:" + game.hiddenWord);
             System.exit(0);
         }
     }
@@ -36,121 +36,26 @@ public class Main {
         return curentChar;
     }
 
-    static void compareChars(Gallow gallow1, char inputedChar) {
+    static void compareChars(GameSession game, char inputedChar) {
         int matchCounter = 0;
         String unhiddentWord = "";
-        for (int i = 0; i < gallow1.hiddenWord.length(); i++) {
-            if (gallow1.hiddenWord.charAt(i) == inputedChar) {
+        for (int i = 0; i < game.hiddenWord.length(); i++) {
+            if (game.hiddenWord.charAt(i) == inputedChar) {
                 unhiddentWord += inputedChar;
                 matchCounter++;
             } else {
-                unhiddentWord += gallow1.unhideenWord.charAt(i);
+                unhiddentWord += game.unhideenWord.charAt(i);
             }
         }
-        if (matchCounter == 0) gallow1.errors++;
-        gallow1.unhideenWord = unhiddentWord;
-        gallow1.usedChar += inputedChar;
+        if (matchCounter == 0) game.errors++;
+        game.unhideenWord = unhiddentWord;
+        game.usedChar += inputedChar;
     }
 
-    static void interfase(Gallow gallow1) {
-
-        switch (gallow1.errors) {
-            case 0:
-                System.out.println("______");
-                break;
-            case 1:
-                System.out.println("|");
-                System.out.println("|");
-                System.out.println("|");
-                System.out.println("|");
-                System.out.println("______");
-                break;
-            case 2:
-                System.out.println("+-----+");
-                System.out.println("|");
-                System.out.println("|");
-                System.out.println("|");
-                System.out.println("|");
-                System.out.println("______");
-                break;
-            case 3:
-                System.out.println("+-----+");
-                System.out.println("|     |");
-                System.out.println("|");
-                System.out.println("|");
-                System.out.println("|");
-                System.out.println("______");
-                break;
-            case 4:
-                System.out.println("+-----+");
-                System.out.println("|     |");
-                System.out.println("|     O");
-                System.out.println("|");
-                System.out.println("|");
-                System.out.println("______");
-                break;
-            case 5:
-                System.out.println("+-----+");
-                System.out.println("|     |");
-                System.out.println("|     O");
-                System.out.println("|    /-\\");
-                System.out.println("|");
-                System.out.println("______");
-
-                break;
-            case 6:
-                System.out.println("Вы проиграли.");
-                System.out.println("+-----+");
-                System.out.println("|     |");
-                System.out.println("|     O");
-                System.out.println("|    /-\\");
-                System.out.println("|     |");
-                System.out.println("______");
-                break;
-        }
-        System.out.println("Загадано слово:" + gallow1.unhideenWord);
-        System.out.println("Вы уже использовали: " + gallow1.usedChar);
-    }
-
-//end Main
 }
 
 
-class Gallow {
-    int errors = 0;
-    String hiddenWord = "";
-    String unhideenWord = "";
-    String usedChar = "";
-
-    void convencedWord() {
-        //***
-        try {
-            File file = new File("src\\vocabular.txt");
-            Scanner scanner = new Scanner(file);
-            Random random = new Random();
-            int randWordNumber = random.nextInt(949);
-            for (int i = 0; scanner.hasNextLine(); i++) {
-                String line = scanner.nextLine();
-                if (i == randWordNumber) hiddenWord = line.toUpperCase();
-            }
-            scanner.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    void generateUnhiddenWord() {
-        for (int i = 0; i < hiddenWord.length(); i++) {
-            unhideenWord += '*';
-        }
-    }
 
 
-    //
-
-
-    //
-//end Gallow
-}
 
 
--- src/Show.java
+++ src/Show.java
@@ -45,10 +45,14 @@ class Show {
                     "----------"}
     };
 
-    public static void showGallow(int errors) {
+    public static void showGallow(GameSession game) {
         for (int i = 0; i < 6; i++) {
-            System.out.println(PICTURES[errors][i]);
+            System.out.println(PICTURES[game.errors][i]);
         }
+        System.out.println(game.usedChar);
+        System.out.println(game.hiddenWord);
+        System.out.println(game.unhideenWord);
     }
+
 
 }
